b3bdceb3190c529daad6036700e0fc63af86ffd6
ttxxly <ttxxly@gmail.com>
2018-08-06 13:48:22 +0800

    add First.java.

---
+++ src/com/darklovy/Number/First.java
@@ -0,0 +1,11 @@
+package com.darklovy.Number;
+
+public class First {
+    private static String s1 = "你好";
+
+    public static void main(String[] args) {
+        String s2 = "Java";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
